7811117e0766f04a8e671ba1786a64ee72ff6b77
en0qi <yknoaki@gmail.com>
2019-02-20 19:56:09 +0900

    modify sendserver (test)

--- genrereq.py
+++ genrereq.py
@@ -6,14 +6,16 @@ from site import *
 
 # コマンドライン引数としてジャンルのキーを受ける
 # pol:政治　eco:経済　int:国際　spo:スポーツ　soc:社会　ent:エンタメ　it:IT　
+if len(argv) < 2:
+	print("実行方法: python3 genrereq.py [pol:政治　eco:経済　int:国際　spo:スポーツ　soc:社会　ent:エンタメ　it:IT]")
 
-genre = sys.argv[1]
-jsonname = "json/"+genre+".json"
-f = open(jsonname, 'r')
+f = open("json/" + sys.argv[1] + ".json", 'r')
 json_dict = json.load(f)
+
 for target in json_dict:
     script = "site/" + format(json_dict[target]["script"])
     site_url = format(json_dict[target]["url"])
     print(script)
     subprocess.call(["python3", script, site_url])
+
 print("fin")
