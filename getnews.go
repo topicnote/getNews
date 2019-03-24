495f25e9b33826bcd52d3ac0fe3496a844bc0eaa
Rparticle <ri9.py1201@gmail.com>
2019-03-24 16:51:20 +0900

    make NewsStruct Public

--- getnews.go
+++ getnews.go
@@ -4,24 +4,24 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-type newsStruct struct {
+type NewsStruct struct {
 	id    int
 	title string
 	url   string
 }
 
-var articles []newsStruct
+var articles []NewsStruct
 
-func Getnews() []newsStruct {
+func Getnews() []NewsStruct {
 	fp := gofeed.NewParser()
 
 	feed, _ := fp.ParseURL("http://www.news24.jp/rss/index.rdf")
 	items := feed.Items
 
-	var newsSlice []newsStruct
+	var newsSlice []NewsStruct
 
 	for index, item := range items { //sliceにappend
-		var news newsStruct
+		var news NewsStruct
 		news.title = item.Title
 		news.url = item.Link
 		news.id = index
