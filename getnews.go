1be08538277a00bb00d8fce0855c64a2f6bf4bdf
Rparticle <ri9.py1201@gmail.com>
2019-03-27 16:29:20 +0900

    コンストラクタにgetKeywordを追加

--- getnews.go
+++ getnews.go
@@ -1,31 +1,43 @@
-package getnews
+package main
 
 import (
 	"github.com/mmcdole/gofeed"
 )
 
+//NewsStruct 記事の構造体
 type NewsStruct struct {
-	Id    int
-	Title string
-	Url   string
+	ID      int
+	Title   string
+	URL     string
+	Keyword map[string]float64
+}
+
+func makeArticle(id int, item *gofeed.Item) NewsStruct {
+	var news NewsStruct
+	news.ID = id
+	news.Title = item.Title
+	news.URL = item.Link
+	// fmt.Println(news.Title)
+	// fmt.Println(item.Description)
+	news.Keyword = makeTopic.getKeyword(news.Title, item.Description)
+	return news
 }
 
 var articles []NewsStruct
 
+//Getnews 構造体にニュース内容をマッピングして返す関数
 func Getnews() []NewsStruct {
 	fp := gofeed.NewParser()
 
-	feed, _ := fp.ParseURL("http://www.news24.jp/rss/index.rdf")
+	// feed, _ := fp.ParseURL("http://www.news24.jp/rss/index.rdf")
+	feed, _ := fp.ParseURL("https://www3.nhk.or.jp/rss/news/cat0.xml")
 	items := feed.Items
 
 	var newsSlice []NewsStruct
 
 	for index, item := range items { //sliceにappend
-		var news NewsStruct
-		news.Title = item.Title
-		news.Url = item.Link
-		news.Id = index
-		newsSlice = append(newsSlice, news)
+		article := makeArticle(index, item)
+		newsSlice = append(newsSlice, article)
 	}
 	return newsSlice
 }
